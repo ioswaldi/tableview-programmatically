9299f6e2776cf830f9e1117d4c4ccf2da42fb255
waldi <febrianda.waldi@gmail.com>
2023-12-04 13:05:07 +0700

    Initial Commit

---
+++ tableview programmatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableview programmatically
+//
+//  Created by Waldi Febrianda on 04/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Table View"
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
